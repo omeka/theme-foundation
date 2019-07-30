7e780afa87fa058cac62c98d478b2357e771c799
Kim Nguyen <design@kimisgold.com>
2019-07-30 14:25:48 -0400

    Fix footer content.

--- common/footer.php
+++ common/footer.php
@@ -6,11 +6,15 @@
     <footer role="contentinfo">
 
         <div id="footer-text">
-            <?php echo get_theme_option('Footer Text'); ?>
+            <?php if ($footerText = get_theme_option('Footer Text')): ?>
+            <?php echo $footerText; ?>
+            <?php else: ?>
+            <p><?php echo __('Proudly powered by <a href="http://omeka.org">Omeka</a>.'); ?></p>
+            <?php endif; ?>
+            
             <?php if ((get_theme_option('Display Footer Copyright') == 1) && $copyright = option('copyright')): ?>
                 <p><?php echo $copyright; ?></p>
             <?php endif; ?>
-            <p><?php echo __('Proudly powered by <a href="http://omeka.org">Omeka</a>.'); ?></p>
         </div>
 
         <?php fire_plugin_hook('public_footer', array('view' => $this)); ?>
