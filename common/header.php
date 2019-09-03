a466dfed30525f07c9cce16a30b5ae8142dcad7a
Kim Nguyen <design@kimisgold.com>
2019-09-03 14:44:28 -0400

    Add image files and style header.

--- common/header.php
+++ common/header.php
@@ -37,6 +37,7 @@
       'integrity' => 'sha256-/PFxCnsMh+nTuM0k3VJCRch1gwnCfKjaP8rJNq5SoBg= sha384-9ksAFjQjZnpqt6VtpjMjlp2S0qrGbcwF/rvrLUg2vciMhwc1UJJeAAOLuJ96w+Nj sha512-UMSn6RHqqJeJcIfV1eS2tPKCjzaHkU/KqgAnQ7Nzn0mLicFxaVhm9vq7zG5+0LALt15j1ljlg8Fp9PT1VGNmDw==',
       'crossorigin' => 'anonymous',
     ));
+    queue_js_url('//fonts.googleapis.com/css?family=Alegreya+Sans|Playfair+Display+SC:900&display=swap');
     echo head_js();
     ?>
 </head>
