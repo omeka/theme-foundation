77481308e4da7821bfcf35a03b72ff2bd7bf87ef
Kim Nguyen <design@kimisgold.com>
2020-06-10 20:11:45 -0400

    Restructure item browse layout.

--- items/browse.php
+++ items/browse.php
@@ -1,5 +1,8 @@
 <?php
 $layout = (get_theme_option('item_browse_layout') !== null) ? get_theme_option('item_browse_layout') : 'list';
+$gridState = ($layout == 'togglegrid') ? 'disabled' : '';
+$listState = ($layout == 'togglelist') ? 'disabled': '';
+$isGrid = (!isset($layout) || strpos($layout, 'grid') !== false) ? true : false;
 $pageTitle = __('Browse Items');
 queue_js_file('browse');
 echo head(array('title' => $pageTitle, 'bodyclass' => 'items browse ' . $layout));
@@ -16,7 +19,11 @@ echo head(array('title' => $pageTitle, 'bodyclass' => 'items browse ' . $layout)
     </div>
 </nav>
 
-<div class="browse-controls">
+<div class="browse-control-mobile">
+<button type="button" class="browse-toggle closed">Tools</button>
+</div>
+
+<div class="browse-controls closed">
     <div class="top-bar-left">
         <?php echo pagination_links(); ?>
     </div>
@@ -35,54 +42,31 @@ echo head(array('title' => $pageTitle, 'bodyclass' => 'items browse ' . $layout)
     </div>
 </div>
 
-<?php if ($layout == 'grid'): ?>
-    <div id="resource-list" class="grid-x grid-margin-x grid-layout">
-    <?php foreach (loop('items') as $item): ?>
-    <div class="item cell small-12 medium-6 large-3">
-        <?php if (metadata('item', 'has files')): ?>
-            <?php echo link_to_item(item_image(), array('class' => 'item-image')); ?>
-        <?php endif; ?>    
-        <div class="item-meta">
-            <h4><?php echo link_to_item(metadata('item', array('Dublin Core', 'Title')), array('class' => 'permalink')); ?></h4>        
-            <?php if ($description = metadata('item', array('Dublin Core', 'Description'), array('snippet' => 250))): ?>
-            <div class="item-description">
-                <?php echo $description; ?>
-            </div>
-            <?php endif; ?>
-            <?php if (metadata('item', 'has tags')): ?>
-            <div class="tags"><span class="tags label"><?php echo __('Tags'); ?></span> <?php echo tag_string('items'); ?></div>
-            <?php endif; ?>
-            <?php fire_plugin_hook('public_items_browse_each', array('view' => $this, 'item' => $item)); ?>
-        </div>
+<ul class="resources <?php echo ($isGrid) ? 'resource-grid' : 'resource-list'; ?>">
+<?php foreach (loop('items') as $item): ?>
+<li class="item resource <?php echo ($isGrid) ? '' : 'media-object'; ?>">
+    <?php if (metadata('item', 'has files')): ?>
+    <div class="resource-image <?php echo ($isGrid) ? '' : 'media-object-section'; ?>">
+        <?php echo link_to_item(item_image(), array('class' => 'thumbnail')); ?>
     </div>
-    <?php endforeach; ?>
-    </div>
-<?php else: ?>
-    <div class="resource-list list-layout">
-    <?php foreach (loop('items') as $item): ?>
-    <div class="item">
-        <h2><?php echo link_to_item(metadata('item', array('Dublin Core', 'Title')), array('class' => 'permalink')); ?></h2>
-        <?php if (metadata('item', 'has files')): ?>
-        <?php echo link_to_item(item_image()); ?>
-        <?php endif; ?>
-        <?php if ($description = metadata('item', array('Dublin Core', 'Description'), array('snippet' => 250))): ?>
-        <div class="item-description">
-            <?php echo $description; ?>
-        </div>
-        <?php endif; ?>
-
+    <?php endif; ?>
+    <?php if ($description = metadata('item', array('Dublin Core', 'Description'), array('snippet' => 250))): ?>
+    <div class="resource-meta <?php echo ($isGrid) ? '' : 'media-object-section'; ?>">
+        <h4><?php echo link_to_item(metadata('item', array('Dublin Core', 'Title')), array('class' => 'permalink')); ?></h4>
+        <div class="description"><?php echo $description; ?></div>
         <?php if (metadata('item', 'has tags')): ?>
         <div class="tags"><span class="tags label"><?php echo __('Tags'); ?></span> <?php echo tag_string('items'); ?></div>
         <?php endif; ?>
     
         <?php fire_plugin_hook('public_items_browse_each', array('view' => $this, 'item' => $item)); ?>
-    </div>
-    <?php endforeach; ?>
-    </div>
-<?php endif; ?>
-
+        </div>
+    <?php endif; ?>
+</li>
+<?php endforeach; ?>
+</ul>
+<div class="browse-controls">
 <?php echo pagination_links(); ?>
-
+</div>
 <div id="outputs">
     <span class="outputs-label"><?php echo __('Output Formats'); ?></span>
     <?php echo output_format_list(false); ?>
--- javascripts/browse.js
+++ javascripts/browse.js
@@ -1,7 +1,16 @@
-(function ($) {
-  $(window).load(function() {
-      $('#resource-list').masonry({
-        itemSelector: '#resource-list .card',
-      });      
-  });
+(function($) {
+    $(document).ready(function() {
+        $('.layout-toggle button').click(function() {
+            $('.layout-toggle button:disabled').removeAttr('disabled');
+            $(this).attr('disabled', true);
+            $('.resources').toggleClass('resource-list').toggleClass('resource-grid');
+            $('.resources .resource').toggleClass('media-object');
+            $('.resource-meta, .resource-image').toggleClass('media-object-section');
+        });
+        
+        $('.browse-toggle').click(function() {
+            $('.browse-controls').toggleClass('open closed');
+            $(this).toggleClass('open closed');
+        });
+    });
 })(jQuery)
