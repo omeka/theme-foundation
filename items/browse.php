646f6e9035db8172d42054dcfc83e8f00d5bbd6d
Kim Nguyen <design@kimisgold.com>
2020-07-03 14:24:13 -0400

    Fix description conditional in the browse.

--- items/browse.php
+++ items/browse.php
@@ -56,17 +56,16 @@ echo head(array('title' => $pageTitle, 'bodyclass' => 'items browse ' . $layout)
         <?php echo link_to_item(item_image(), array('class' => 'thumbnail')); ?>
     </div>
     <?php endif; ?>
-    <?php if ($description = metadata('item', array('Dublin Core', 'Description'), array('snippet' => 250))): ?>
     <div class="resource-meta <?php echo ($isGrid) ? '' : 'media-object-section'; ?>">
         <h4><?php echo link_to_item(metadata('item', array('Dublin Core', 'Title')), array('class' => 'permalink')); ?></h4>
+        <?php if ($description = metadata('item', array('Dublin Core', 'Description'), array('snippet' => 250))): ?>
         <div class="description"><?php echo $description; ?></div>
+        <?php endif; ?>
         <?php if (metadata('item', 'has tags')): ?>
         <div class="tags"><span class="tags label"><?php echo __('Tags'); ?></span> <?php echo tag_string('items'); ?></div>
-        <?php endif; ?>
-    
+        <?php endif; ?>    
         <?php fire_plugin_hook('public_items_browse_each', array('view' => $this, 'item' => $item)); ?>
-        </div>
-    <?php endif; ?>
+    </div>
 </li>
 <?php endforeach; ?>
 </ul>
