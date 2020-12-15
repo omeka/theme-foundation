d79acc1285f67b35fbee4e62ed8f7fcbe18aa487
Kim Nguyen <design@kimisgold.com>
2020-12-15 12:01:09 -0500

    Rely on core js for skipnav functionality.

--- javascripts/app.js
+++ javascripts/app.js
@@ -3,15 +3,5 @@
       $('.search-toggle').click(function() {
         $('#search-container').toggleClass('open').toggleClass('closed');
       })
-      
-      $('#skipnav').keydown(function(e) {
-        if (e.keyCode == 13) {
-            $('#content').attr('tabindex', '-1').focus();
-        }
-      });
-      
-      $('#content').on('blur focusout', function() {
-        $(this).removeAttr('tabindex');
-      });
   });
 })(jQuery)
