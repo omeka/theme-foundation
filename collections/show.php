f7ad154806d209ae2f923a279bff202d4351823d
Kim Nguyen <design@kimisgold.com>
2021-08-10 14:57:45 -0400

    Fix wrapping with search toggle.

--- collections/show.php
+++ collections/show.php
@@ -18,6 +18,7 @@ $totalItems = metadata('collection', 'total_items');
 <div id="collection-items">
 
     <h3><?php echo __('Collection Items'); ?></h3>
+    <?php if ($totalItems > 0): ?>
     <div class="browse-control-mobile">
     <button type="button" class="browse-toggle closed">Tools</button>
     </div>
@@ -31,7 +32,6 @@ $totalItems = metadata('collection', 'total_items');
         </div>
         <?php endif; ?>
         <div class="top-bar-right">
-            <?php if ($totalItems > 0): ?>
             <?php
             $sortLinks[__('Title')] = 'Dublin Core,Title';
             $sortLinks[__('Creator')] = 'Dublin Core,Creator';
@@ -41,36 +41,36 @@ $totalItems = metadata('collection', 'total_items');
                 <span class="sort-label"><?php echo __('Sort by: '); ?></span><?php echo browse_sort_links($sortLinks); ?>
             </div>       
                  
-            <?php endif; ?>
         </div>
     </div>
     <ul class="resources <?php echo ($isGrid) ? 'resource-grid' : 'resource-list'; ?>">
-    <?php foreach (loop('items') as $item): ?>
-    <li class="item resource <?php echo ($isGrid) ? '' : 'media-object'; ?>">
-        <?php if (metadata('item', 'has files')): ?>
-        <div class="resource-image <?php echo ($isGrid) ? '' : 'media-object-section'; ?>">
-            <?php echo link_to_item(item_image(), array('class' => 'thumbnail')); ?>
-        </div>
-        <?php endif; ?>
-        <div class="resource-meta <?php echo ($isGrid) ? '' : 'media-object-section'; ?>">
-            <h4><?php echo link_to_item(metadata('item','rich_title', array('no_escape' => true)), array('class' => 'permalink')); ?></h4>
-            <?php if ($description = metadata('item', array('Dublin Core', 'Description'))): ?>
-            <div class="description <?php echo $truncateDescription; ?>"><?php echo $description; ?></div>
-            <?php endif; ?>
-            <?php if (metadata('item', 'has tags')): ?>
-            <div class="tags"><span class="tags label"><?php echo __('Tags'); ?></span> <?php echo tag_string('items'); ?></div>
-            <?php endif; ?>    
+        <?php foreach (loop('items') as $item): ?>
+            <li class="item resource <?php echo ($isGrid) ? '' : 'media-object'; ?>">
+                <?php if (metadata('item', 'has files')): ?>
+                    <div class="resource-image <?php echo ($isGrid) ? '' : 'media-object-section'; ?>">
+                        <?php echo link_to_item(item_image(), array('class' => 'thumbnail')); ?>
+                    </div>
+                    <?php endif; ?>
+                    <div class="resource-meta <?php echo ($isGrid) ? '' : 'media-object-section'; ?>">
+                        <h4><?php echo link_to_item(metadata('item','rich_title', array('no_escape' => true)), array('class' => 'permalink')); ?></h4>
+                        <?php if ($description = metadata('item', array('Dublin Core', 'Description'))): ?>
+                            <div class="description <?php echo $truncateDescription; ?>"><?php echo $description; ?></div>
+                            <?php endif; ?>
+                            <?php if (metadata('item', 'has tags')): ?>
+                                <div class="tags"><span class="tags label"><?php echo __('Tags'); ?></span> <?php echo tag_string('items'); ?></div>
+                                <?php endif; ?>    
             <?php fire_plugin_hook('public_items_browse_each', array('view' => $this, 'item' => $item)); ?>
         </div>
     </li>
     <?php endforeach; ?>
     </ul>
-    <?php if ($totalItems > 0): ?>
-        <div id="item-list-footer">
-            <?php echo link_to_items_browse(__(plural('View item', 'View all %s items', $totalItems), $totalItems), array('collection' => metadata('collection', 'id')), array('class' => 'view-items-link')); ?>
-        </div>
-    <?php endif; ?>
+    <div id="item-list-footer">
+        <?php echo link_to_items_browse(__(plural('View item', 'View all %s items', $totalItems), $totalItems), array('collection' => metadata('collection', 'id')), array('class' => 'view-items-link')); ?>
+    </div>
 </div><!-- end collection-items -->
+<?php else: ?>
+    <p><?php echo __('You have no items.'); ?></p>
+<?php endif; ?>
 
 <?php fire_plugin_hook('public_collections_show', array('view' => $this, 'collection' => $collection)); ?>
 
